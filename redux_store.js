4779077091be03fb41ff7832e1a8785d3e1e4dc7
Peizhi Zheng <pzheng16@asu.edu>
2020-09-09 23:02:47 -0700

    updating state

--- redux_store.js
+++ redux_store.js
@@ -1,11 +1,41 @@
+
+{
+    type: 'ADD_TODO',
+    todo: {
+        id: 0,
+        name: 'Learn Redux',
+        complete: false,
+    }
+}
+{
+    type:'REMOVE_TODO',
+    id:0,
+}
+{
+    type: 'TOGGLE_TODO',
+    id:0,
+}
+
+
+{
+    type: 'ADD_GOAL',
+    goal: {
+        id: 0,
+        name: 'Run a Marathon'
+    }
+}
+{
+    type: 'REMOVE_GOAL',
+    id: 0
+}
+
 function createStore (){
   //the store should have four parts
   //1. the state
   //2. Get the state
   //3. Listen to changes on the state.
   //4. Update the state
-
-  let state;
+  let state // state of the entire application. 
   let listeners = [] //array of functions
   const getState = () => state
 
@@ -16,7 +46,6 @@ function createStore (){
         listeners.filter((l) => l !== listener)
     }
   }
-
   return {
     getState,
     subscribe,
